b2160cf7fe0876bbfdff85a169ee0b6c31b0aff0
Thomas Knauth <thomas.knauth@gmx.de>
2025-06-24 14:58:54 +0200

    y=2023, d=8, part=2

--- 2023-08.py
+++ 2023-08.py
@@ -46,7 +46,55 @@ ZZZ = (ZZZ, ZZZ)
 
 # Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
 
+# --- Part Two ---
+
+# The sandstorm is upon you and you aren't any closer to escaping the wasteland. You had the camel
+# follow the instructions, but you've barely left your starting position. It's going to take
+# significantly more steps to escape!
+
+# What if the map isn't for people - what if the map is for ghosts? Are ghosts even bound by the
+# laws of spacetime? Only one way to find out.
+
+# After examining the maps a bit longer, your attention is drawn to a curious fact: the number of
+# nodes with names ending in A is equal to the number ending in Z! If you were a ghost, you'd
+# probably just start at every node that ends with A and follow all of the paths at the same time
+# until they all simultaneously end up at nodes that end with Z.
+
+# For example:
+
+test_input_part2='''\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+'''
+
+# Here, there are two starting nodes, 11A and 22A (because they both end with A). As you follow each
+# left/right instruction, use that instruction to simultaneously navigate away from both nodes
+# you're currently on. Repeat this process until all of the nodes you're currently on end with
+# Z. (If only some of the nodes you're on end with Z, they act like any other node and you continue
+# as normal.) In this example, you would proceed as follows:
+
+# Step 0: You are at 11A and 22A.
+# Step 1: You choose all of the left paths, leading you to 11B and 22B.
+# Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+# Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+# Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+# Step 5: You choose all of the left paths, leading you to 11B and 22C.
+# Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+# So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+
+# Simultaneously start on every node that ends with A. How many steps does it take before you're
+# only on nodes that end with Z?
+
 import io
+import math
 import sys
 
 def parse_input(f):
@@ -65,16 +113,54 @@ def part1(f):
     moves, m = parse_input(f)
     pos = 'AAA'
     c = 0
+
     while True:
-        for move in moves:
+        for (i, move) in enumerate(moves):
             assert move in ['L', 'R']
-            pos = m[pos][0] if move == 'L' else m[pos][1]
+            # Array indexing via True(1)/False(0) ¯\_(ツ)_/¯
+            pos = m[pos][move == 'R']
             c += 1
             if pos == 'ZZZ':
                 return c
 
+def solve_one(loc, moves, m):
+    c = 0
+    states = set()
+
+    while True:
+        for (idx, move) in enumerate(moves):
+            assert move in ['L', 'R']
+            states.add((loc, idx))
+            if loc[-1] == 'Z':
+                return c
+            loc = m[loc][0] if move == 'L' else m[loc][1]
+            c += 1
+
 def part2(f):
-    return 0
+
+    moves, m = parse_input(f)
+
+    # Lemma 1: Cycles can only appear after having found at least one solution, i.e., if there is a
+    # cycle before a solution is found, there is no solution.
+
+    # Lemma 2: All graphs are independent of each other
+    # print(sum([len(loc2states[k]) for k in loc2states.keys()]))
+    # print(len(set().union(*loc2states.values())))
+
+    # Lemma 3: Start states have no incoming edges.
+
+    # Lemma 4: Periodicity of loop is identical to # of steps from start state till end state (I
+    # guess it has to be this way if we want to solve using least common multiple).
+
+    # Lemma 5: There is only one start and end state for each cycle.
+
+    locs = [key for key in m.keys() if key[-1] == 'A']
+
+    cycles_lens = []
+    for loc in locs:
+        cycles_lens.append(solve_one(loc, moves, m))
+
+    return math.lcm(*cycles_lens)
 
 if __name__ == '__main__':
 
@@ -84,5 +170,5 @@ if __name__ == '__main__':
 
     print(part1(io.StringIO(test_input)))
     print(part1(open(fn)))
-    #print(part2(io.StringIO(test_input)))
-    #print(part2(open(fn)))
+    print(part2(io.StringIO(test_input_part2)))
+    print(part2(open(fn)))
